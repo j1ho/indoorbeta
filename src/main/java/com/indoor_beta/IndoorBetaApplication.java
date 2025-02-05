b97ec7789d6f1d847e035e71055810506a98e18b
Jason <jasonho49@gmail.com>
2025-02-05 17:38:28 -0500

    Generated Springboot Project

---
+++ src/main/java/com/indoor_beta/IndoorBetaApplication.java
@@ -0,0 +1,13 @@
+package com.indoor_beta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndoorBetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndoorBetaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/indoor_beta/IndoorBetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indoor_beta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndoorBetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
